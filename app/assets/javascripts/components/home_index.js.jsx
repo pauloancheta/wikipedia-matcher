159e2dd214bf83eaa88125e62acf6a5c85b2d36d
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-06-03 16:11:05 -0700

    able to search from wikipedia.   - TODO: create a search box, add to database then styling

--- app/assets/javascripts/components/home_index.js.jsx
+++ app/assets/javascripts/components/home_index.js.jsx
@@ -1,4 +1,19 @@
 var HomeIndex = React.createClass({
+  componentDidMount: function() {
+    title = "jimi hendrix"
+    $.getJSON("http://en.wikipedia.org/w/api.php?&callback=?",{
+      srsearch: title,
+      action: 'query',
+      list: 'search',
+      format: 'json'
+    },
+    function(data) {
+      $.each(data.query.search, function(i, item){
+        console.log(item);
+      })
+    });
+  },
+
   render: function() {
     return(
       <div>
