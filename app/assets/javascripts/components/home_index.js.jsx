c32727a1e15dae9ac7d51df97e7aa530f998d0b6
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-06-03 16:23:38 -0700

    search function added.   - TODO: link to the original post, and add to favourites

--- app/assets/javascripts/components/home_index.js.jsx
+++ app/assets/javascripts/components/home_index.js.jsx
@@ -1,6 +1,13 @@
 var HomeIndex = React.createClass({
-  componentDidMount: function() {
-    title = "jimi hendrix"
+  getInitialState: function(){
+    return {searchResults: []}
+  },
+
+  onSubmitHandler: function(e) {
+    e.preventDefault();
+    var title = React.findDOMNode(this.refs.search).value + '';
+    var self = this;
+
     $.getJSON("http://en.wikipedia.org/w/api.php?&callback=?",{
       srsearch: title,
       action: 'query',
@@ -8,17 +15,26 @@ var HomeIndex = React.createClass({
       format: 'json'
     },
     function(data) {
-      $.each(data.query.search, function(i, item){
-        console.log(item);
-      })
+      self.setState({searchResults: data.query.search})
+      // $.each(data.query.search, function(i, item){
+      //   console.log(encodeURIComponent(item.title));
+      // })
     });
   },
 
   render: function() {
+    results = []
+    this.state.searchResults.forEach(function(result){
+      results.push(result.title)
+    });
     return(
       <div>
-        <h1>Hello world! </h1>
+        <form onSubmit={this.onSubmitHandler}>
+          <input type="text" ref="search" placeholder="Search an Wikipedia!"></input>
+          <input type="submit"></input>
+        </form>
+        {results}
       </div>
     )
   }
-})
+});
